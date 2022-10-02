6849c3ba1df0247dbea359c2a5de73c044000a8d
Saoa35 <saoamah@gmail.com>
2022-10-02 22:40:02 +0200

    created users_invitation-app

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,8 +1,13 @@
-import './App.css';
+import React from 'react';
+import '../../index.scss';
+
+// Тут список пользователей: https://reqres.in/api/users
 
 function App() {
   return (
     <div className="App">
+      <Users />
+      {/* <Success /> */}
     </div>
   );
 }
