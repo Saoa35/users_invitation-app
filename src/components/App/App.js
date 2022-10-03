9bba183b21a13709ba7813b7e2ea8f94a853b9ed
Saoa35 <saoamah@gmail.com>
2022-10-03 19:59:27 +0200

    + useState

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../../index.scss';
 
 // Тут список пользователей: https://reqres.in/api/users
 
 function App() {
+
+  const [users, setUsers] = useState([]);
+
   return (
     <div className="App">
-      {/* <Users /> */}
-      {/* <Success /> */}
+      
     </div>
   );
 }
