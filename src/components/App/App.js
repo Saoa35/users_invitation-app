6f9efc1f8f93f36c4ad146a0da813c7b072031ec
Saoa35 <saoamah@gmail.com>
2022-10-03 20:23:50 +0200

    + useEffect

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,21 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import '../../index.scss';
 
-// Тут список пользователей: https://reqres.in/api/users
+const url = 'https://reqres.in/api/users';
 
 function App() {
 
   const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch(url)
+      .then(res => res.json())
+      .then(json => {
+        setUsers(json.data)
+
+        console.log(users)
+      })
+  }, []);
 
   return (
     <div className="App">
